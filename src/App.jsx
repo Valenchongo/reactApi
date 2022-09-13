327688880a6a5913a5cc36a81da48feb79392c5a
Valenchongo <107526652+Valenchongo@users.noreply.github.com>
2022-09-13 13:00:10 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter, Routes, Route} from "react-router-dom"
+import {BrowserRouter, Routes, Route,HashRouter} from "react-router-dom"
 import './App.css';
 import Personaje from "./components/personaje"
 import Inicio from "./components/inicio"
@@ -7,14 +7,14 @@ import Logo from "./imagenes/logo.png"
 function App() {
   return (
     <div >
-    <BrowserRouter>
+    <HashRouter hashType="hashbang">
     <header> <div className="logo"> <img className="logo" src={Logo}  /></div></header>
      <Routes>
       <Route path="/:pagina" element={<Inicio></Inicio>}> </Route>
    
       <Route path="/personajes/:id/:page" element={<Personaje></Personaje>}> </Route>
      </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </div>
   );
 }
